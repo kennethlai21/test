80126c391343db52041038dea2058d3a2fac7779
kennethlai21 <kla83@sfu.ca>
2015-01-13 12:02:19 -0800

    initial commit

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name routeApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the routeApp
+ */
+angular.module('routeApp')
+  .controller('ContactCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/contract.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myappApp.controller:ContractCtrl
+ * @description
+ * # ContractCtrl
+ * Controller of the myappApp
+ */
+angular.module('myappApp')
+  .controller('ContractCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
